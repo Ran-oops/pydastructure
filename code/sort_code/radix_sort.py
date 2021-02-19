ad16a4761fd62505aeb7c351005194004de40034
sunyan01 <sunyan01@ehousechina.com>
2021-02-19 18:32:02 +0800

    update

--- code/sort_code/radix_sort.py
+++ code/sort_code/radix_sort.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def radix_sort(s):
+def radix_sort(alist):
     """基数排序"""
     i = 0 # 记录当前正在排拿一位，最低位为1
-    max_num = max(s)  # 最大值
+    max_num = max(alist)  # 最大值
     j = len(str(max_num))  # 记录最大值的位数
     while i < j:
         bucket_list =[[] for _ in range(10)] #初始化桶数组
-        for x in s:
+        for x in alist:
             bucket_list[int(x / (10**i)) % 10].append(x) # 找到位置放入桶数组
         print(bucket_list)
-        s.clear()
+        alist.clear()
         for x in bucket_list:   # 放回原序列
             for y in x:
-                s.append(y)
+                alist.append(y)
         i += 1
 
 if __name__ == '__main__':
